56bddaa8993e6313e8d688ff869e081df5e32a39
ImanF01 <imanfahmy01@gmail.com>
2018-09-27 18:48:55 -0700

    editted main storyboard

--- BookSample/BookSample/ViewController.swift
+++ BookSample/BookSample/ViewController.swift
@@ -14,7 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    guard let jsonURL = Bundle.main.url(forResource: "bookSample", withExtension: "json") else {
+    print("Could not find Random-User.json!")
+    return
+    }
+    let jsonData = try! Data(contentsOf: jsonURL)
 
-
+    @IBOutlet weak var bookCoverImage: UIImageView!
+    @IBOutlet weak var titleName: UILabel!
+    @IBOutlet weak var authorName: UILabel!
+    @IBOutlet weak var isbnNumber: UILabel!
+    @IBOutlet weak var textSummary: UITextView!
 }
 
